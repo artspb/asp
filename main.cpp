c6f015964732f195a16931aa61c2a39081b4749a
Artem Khvastunov <art.spb@gmail.com>
2010-09-15 14:26:02 +0400

    Added mult::get.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <QtCore/QCoreApplication>
+#include <QDebug>
+#include "mult.h"
 
 int main(/*int argc, char *argv[]*/)
 {
     //QCoreApplication a(argc, argv);
+
+    double * p = new double;
+    double * q = new double;
+    int n;
+
+    n = 2;
+
+    for(int i = 1; i <= n; i++)
+    {
+        p[i] = 0,2;
+        q[i] = 0,2;
+    }
+
+    mult poly = mult(p, q, n);
+
+    qDebug() << poly.get()[0];
 
     return 0;
 }
--- mult.cpp
+++ mult.cpp
@@ -1,5 +1,34 @@
 #include "mult.h"
 
-mult::mult()
+mult::mult(double * newP, double * newQ, int newN)
 {
+    p = newP;
+    q = newQ;
+    n = newN;
+}
+
+int mult::count()
+{
+    return n;
+}
+
+double * mult::get()
+{
+    double * z = new double;
+
+    //Алгоритм для рассчета коэффициентов при степенях z.
+    //Возвращает значения только для полинома степени n.
+    z[0] = q[1];
+    z[1] = p[1];
+    for(int i = 2; i <= n; i++)
+    {
+        z[i] = 0;
+        for(int j = i; j > 0; j--)
+        {
+            z[j] = z[j] * q[i] + z[j-1] * p[i];
+        }
+        z[0] = z[0] * q[i];
+    }
+
+    return z;
 }
