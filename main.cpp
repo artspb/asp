a8e9947e39f1a22cc01767d78a0b3c570e4c2a58
Artem Khvastunov <art.spb@gmail.com>
2010-10-13 18:20:57 +0400

    Added .gitignore file.

--- main.cpp
+++ main.cpp
@@ -2,24 +2,22 @@
 #include <QDebug>
 #include "mult.h"
 
-int main(/*int argc, char *argv[]*/)
+int main(int argc, char *argv[])
 {
-    //QCoreApplication a(argc, argv);
+    QCoreApplication a(argc, argv);
 
     //Создаем и определяем переменные.
-    int n = 3;
+    int const n = 3;
     double * factors;
     mult poly;
 
-    //Поставь TODO плагин наконец! ВотЪ.
-
     factors = poly.get(n);
     if(factors)
-        for(int i = 0; i <=n; i++)
+        for(int i = 0; i <= n; i++)
             qDebug() << factors[i];
 
     //Удаляем более не нужные указатели.
     delete factors;
 
-    return 0;
+    return 0;//a.exec(); //Нужно разобраться, почему нет выхода в случае, когда a.exec().
 }
